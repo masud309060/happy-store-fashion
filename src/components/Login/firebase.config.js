734645c9753194560845d7e7e505ab5771a027e5
masud309060 <masud306090@gmail.com>
2021-03-01 02:28:10 +0600

    update build folder

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: "AIzaSyBd4EGU8796vWMjE6NtzR2ASo4jQDhXm4M",
   authDomain: "happy-store-fashion.firebaseapp.com",
@@ -7,4 +9,7 @@ const firebaseConfig = {
   appId: "1:765578460916:web:0fbd9272b842b8ef4328a1"
 };
 
-export default firebaseConfig;
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+export const db = firebaseApp.firestore()
+export const auth = firebaseApp.auth()
